6bafd127932cde83ad782206c0df11a688f75c74
Paras <paras@openinapp.com>
2024-08-03 16:48:23 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@ import About from "@/components/about";
 import Contact from "@/components/contact";
 import MyProjects from "@/components/MyProjects";
 import Intro from "@/components/intro";
-import Experience from "@/components/Experience";
-import SectionDivider from "@/components/section-divider";
+import Experience from "@/components/experience";
 import Skills from "@/components/skills";
-import { useScroll } from "@/lib/hooks/useScroll";
+
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4 overflow-x-hidden">
       <Intro />
-      {/* <SectionDivider /> */}
       <About />
       <MyProjects />
       <Experience />
