5edc2b98f129bb2c8cd92ce45c253144d8b00473
Aditya jain <aditya@openinapp.com>
2024-08-17 18:08:56 +0530

    fix the project drawer and added the animation

--- components/MyProjects.tsx
+++ components/MyProjects.tsx
@@ -1,123 +1,210 @@
-'use client';
+"use client";
 
-import React from 'react';
-import SectionHeading from './section-heading';
-import TinderCard from 'react-tinder-card';
-import { useSectionInView } from '@/lib/hooks';
-import { cn } from '@/lib/utils';
-import Image from 'next/image';
-import { CardBody, CardContainer, CardItem } from './3d-card-effect';
-import { Drawer, DrawerContent } from './common/Drawer';
+import React, { use, useEffect } from "react";
+import SectionHeading from "./section-heading";
+import TinderCard from "react-tinder-card";
+import { useSectionInView } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { CardBody, CardContainer, CardItem } from "./3d-card-effect";
+import { Drawer, DrawerContent } from "./common/Drawer";
+import { motion } from "framer-motion";
+import Lottie from "react-lottie-player/dist/LottiePlayerLight";
+import loadingLottie from "@/components/animation/loading-lottie.json";
+
+const tinderCards = [
+  {
+    title: "Backend-End Developer",
+    location: "Orlando, FL",
+    description: "Worked on the Google Search Engine",
+  },
+  {
+    title: "Software Engineer",
+    location: "Google",
+    description: "Worked on the Google Search Engine",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description: "Worked on the Google Search Engine",
+  },
+];
 
 export default function MyProjects() {
-  const { ref } = useSectionInView('Experience');
+  const { ref } = useSectionInView("Projects");
   const [detailModal, setDetailModal] = React.useState(false);
   const [selectedCard, setSelectedCard] = React.useState<number>(0);
+  const [swipedCards, setSwipedCards] = React.useState(new Set());
+  const [restore, setRestore] = React.useState(false);
+  const [cards, setCards] = React.useState(tinderCards);
 
-  const onSwipe = (direction: any) => {
-    console.log('You swiped: ' + direction);
+  const onSwipe = (direction: string, identifier: number) => {
+    console.log("You swiped: " + direction);
+    setSwipedCards((prev) => new Set(prev).add(identifier));
   };
 
-  const onCardLeftScreen = (myIdentifier: any) => {
-    console.log(myIdentifier + ' left the screen');
+  const onCardLeftScreen = (identifier: number) => {
+    console.log(identifier + " left the screen");
+    // Optional: Remove the card from the list
+    setCards((prev) => prev.filter((_, index) => index !== identifier));
   };
 
-  const tinderCards = [
-    {
-      title: 'Backend-End Developer',
-      location: 'Orlando, FL',
-      description: 'Worked on the Google Search Engine',
-      color: 'bg-green-500',
-    },
-    {
-      title: 'Software Engineer',
-      location: 'Google',
-      description: 'Worked on the Google Search Engine',
-      color: 'bg-red-500',
-    },
-    {
-      title: 'Front-End Developer',
-      location: 'Orlando, FL',
-      description: 'Worked on the Google Search Engine',
-      color: 'bg-blue-500',
-    },
-  ];
-  //localhost:3000/_next/image?url=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1441974231531-c6227db76b6e%3Fq%3D80%26w%3D2560%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%253D%253D&w=2048&q=75
+  const restoreCards = () => {
+    setCards(tinderCards);
+    setSwipedCards(new Set());
+    setRestore(false);
+  };
 
-  http: return (
-    <section id='projects' ref={ref} className='w-full sm:mb-40'>
+  useEffect(() => {
+    if (swipedCards.size === tinderCards.length) {
+      setRestore(true);
+    }
+  }, [swipedCards]);
+
+  useEffect(() => {
+    if (restore) {
+      setTimeout(() => {
+        restoreCards();
+      }, 3000);
+    }
+  }, [restore]);
+
+  const containerVariants = {
+    hidden: { opacity: 0, scale: 2 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delay: 0.3,
+        duration: 0.4,
+        ease: "easeInOut",
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const childVariants = {
+    hidden: { opacity: 0, scale: 1.5 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        type: "spring",
+        damping: 7,
+      },
+    },
+  };
+
+  return (
+    <section id="projects" ref={ref} className="h-[80dvh] w-full px-4 lg:px-10">
       <SectionHeading>Projects</SectionHeading>
-      <div className='w-full flex flex-col justify-center items-center h-[100vh]'>
-        <div className='w-[300px] h-[450px] lg:w-[400px] lg:h-[550px] relative'>
-          {tinderCards.map((card, index) => (
-            <TinderCard
-              key={index}
-              onSwipe={onSwipe}
-              onCardLeftScreen={() => onCardLeftScreen('fooBar')}
-              preventSwipe={['up', 'down']}
-              className='absolute'
+      <div className="relative flex items-center justify-center h-full w-full">
+        {cards.map((card, index) => (
+          <TinderCard
+            key={index}
+            preventSwipe={["up", "down"]}
+            className="absolute top-16 w-full md:w-[35rem]"
+            onSwipe={(dir) => onSwipe(dir, index)}
+            onCardLeftScreen={() => onCardLeftScreen(index)}
+          >
+            <div
+              className={cn(
+                "w-full px-4 shadow-lg text-black select-none bg-cover bg-center rounded-3xl hover:-translate-y-12 transition-transform duration-300 ease-in-out",
+                {
+                  "rotate-[-4deg]": index === 0,
+                  "rotate-[4deg]": index === 1,
+                }
+              )}
+              style={{
+                transformOrigin: "center 200%",
+              }}
             >
-              <div
-                className={cn(
-                  ' shadow-lg text-black select-none bg-cover bg-center rounded-3xl hover:-translate-y-12 transition-transform duration-300 ease-in-out',
-                  card.color,
-                  {
-                    'rotate-[-4deg]': index === 0,
-                    'rotate-[4deg]': index === 1,
-                  }
-                )}
-                style={{
-                  transformOrigin: 'center 200%',
-                }}
-                onClick={() => {
-                  setSelectedCard(index);
-                  setDetailModal(true);
-                }}
-              >
-                <CardContainer className='inter-var'>
-                  <CardBody className='bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  '>
-                    <CardItem
-                      translateZ='50'
-                      className='text-xl font-bold text-neutral-600 dark:text-white'
-                    >
-                      {card.title}
-                    </CardItem>
-                    <CardItem
-                      as='p'
-                      translateZ='60'
-                      className='text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300'
-                    >
-                      {card.description}
-                    </CardItem>
-                    <CardItem
-                      as='p'
-                      translateZ='60'
-                      className='text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300'
-                    >
-                      {card.location}
-                    </CardItem>
-                    <CardItem translateZ='100' className='w-full mt-4'>
-                      <Image
-                        src='https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                        height='1000'
-                        width='1000'
-                        className='h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl'
-                        alt='thumbnail'
-                      />
-                    </CardItem>
-                  </CardBody>
-                </CardContainer>
-              </div>
-            </TinderCard>
-          ))}
-        </div>
+              <CardContainer className="inter-var w-full">
+                <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full h-[18rem] sm:h-[22rem] lg:h-auto rounded-xl p-4 sm:p-6 border">
+                  <CardItem
+                    translateZ="50"
+                    className="text-lg sm:text-xl font-bold text-neutral-600 dark:text-white"
+                  >
+                    {card.title}
+                  </CardItem>
+                  <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-neutral-500 text-xs sm:text-sm max-w-sm mt-2 dark:text-neutral-300"
+                  >
+                    {card.description}
+                  </CardItem>
+                  <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-neutral-500 text-xs sm:text-sm max-w-sm mt-2 dark:text-neutral-300"
+                  >
+                    {card.location}
+                  </CardItem>
+                  <CardItem
+                    translateZ="100"
+                    className="w-full mt-4"
+                    onClick={() => {
+                      setSelectedCard(index);
+                      setDetailModal(true);
+                    }}
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      className="h-[10rem] sm:h-[14rem] w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                      alt="thumbnail"
+                    />
+                  </CardItem>
+                </CardBody>
+              </CardContainer>
+            </div>
+          </TinderCard>
+        ))}
+        {restore && (
+          <Lottie
+            animationData={loadingLottie}
+            play
+            loop
+            className="absolute h-28 w-28 bg-transparent"
+          />
+        )}
       </div>
+
       <GlassMorphedDrawer open={detailModal} onOpenChange={setDetailModal}>
-        <div>
-          <p>{tinderCards[selectedCard].title}</p>
-          <p>{tinderCards[selectedCard].description}</p>
-          <p>{tinderCards[selectedCard].location}</p>
-        </div>
+        <motion.div
+          className="text-white flex flex-col justify-center items-center gap-10"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.p
+            className="text-3xl font-bold mb-2"
+            variants={childVariants}
+          >
+            {tinderCards[selectedCard].title}
+          </motion.p>
+          <motion.img
+            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="thumbnail"
+            className="rounded-xl w-full h-full lg:w-[500px]"
+          />
+          <motion.p
+            className="text-sm mb-2 text-justify rounded-xl text-white p-4"
+            variants={childVariants}
+          >
+            {/* {tinderCards[selectedCard].description} */}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            aliquet, nisl nec aliquam tempus, purus purus tempor libero, non
+            laoreet nulla nunc sed odio. Nulla facilisi. Proin nec nulla
+            fringilla, interdum odio eget, euismod elit. Nulla facilisi. Proin
+            nec nulla fringilla, interdum odio eget, euism Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Nulla aliquet, nisl nec aliquam
+            tempus, purus purus tempor libero, non laoreet nulla nunc sed odio.
+            Nulla facilisi. Proin nec nulla fringilla, interdum odio eget,
+            euismod elit. Nulla facilisi. Proin nec nulla fringilla, interdum
+            odio eget, euism
+          </motion.p>
+        </motion.div>
       </GlassMorphedDrawer>
     </section>
   );
@@ -128,14 +215,19 @@ type GlassMorphedDrawerProps = {
   onOpenChange: (open: boolean) => void;
   children?: React.ReactNode;
 };
+
 const GlassMorphedDrawer = ({
   open,
   onOpenChange,
   ...props
 }: GlassMorphedDrawerProps) => {
   return (
-    <Drawer open={open} onOpenChange={onOpenChange} direction='right'>
-      <DrawerContent className='h-full w-full bg-red-0 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 '>
+    <Drawer open={open} onOpenChange={onOpenChange} direction="right">
+      <DrawerContent
+        className="h-full w-full bg-white rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-none px-6 py-8"
+        showPill={false}
+        onClick={() => onOpenChange(false)}
+      >
         {props.children}
       </DrawerContent>
     </Drawer>
