d881d585a6f2f8fc4a0d1ee859173eb6098986d2
Paras <paras@openinapp.com>
2024-08-18 21:49:55 +0530

    fix the ui

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import { Lottie } from "./lazy";
+import alien from "@/components/animation/alien.json";
 export default function Footer() {
   return (
-    <footer className=" px-4 text-center text-foreground border-t-white border-t py-5"> 
+    <footer className=" px-4 py-8 text-center text-foreground border-t-white border-t">
       <small className="mb-2 block text-xs">
         &copy; 2030 Paras. All rights reserved.
       </small>
